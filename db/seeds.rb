8813732b35dd82dd213fe63889019fa3c77585cf
Dennis Oelkers <dennis@graylog.com>
2016-10-18 19:06:58 +0200

    Adding initial seeding of database.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'colorize'
+require "homegear"
+
+homegear = Homegear.default
+homegear.getIds.each do |id|
+  name = homegear.getName id
+  puts "Adding device ".colorize(:green) + name.colorize(:blue) + " with id ".colorize(:green) + id.to_s.colorize(:blue)
+  Device.create(id: id, name: name)
+end
